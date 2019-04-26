512bc9019a5dfd8efd989b5fb158bb5731646be2
robsonsilv4 <robsonsilv410@gmail.com>
2019-04-26 14:03:38 -0300

    Detail, delete e ajustes

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from movies.views import *
+from movies.views import MovieCreateView, MovieListView, MovieDetailView, MovieUpdateView, MovieDeleteView
 
+app_name = 'movies'
 urlpatterns = [
-    path('adicionar/', MovieCreateView.as_view(), name='movie-create'),
-    path('listar/', MovieListView.as_view(), name='movie-list'),
-    path('detalhes/', MovieDetailView.as_view(), name='movie-detail'),
-    path('atualizar/', MovieUpdateView.as_view(), name='movie-update'),
-    path('deletar/', MovieDeleteView.as_view(), name='movie-delete'),
+    path('adicionar/', MovieCreateView.as_view(), name='create'),
+    path('listar/', MovieListView.as_view(), name='list'),
+    path('<int:pk>/detalhes/', MovieDetailView.as_view(), name='detail'),
+    path('<int:pk>/atualizar/', MovieUpdateView.as_view(), name='update'),
+    path('<int:pk>/deletar/', MovieDeleteView.as_view(), name='delete'),
 ]
